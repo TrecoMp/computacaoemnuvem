254c92b0208d94aefb688cbd5b8aa010885f3073
Marcos Paulo Fernandes <40579498+TrecoMp@users.noreply.github.com>
2019-06-12 19:29:34 -0300

    Update ScriptWordPressAWS.sh

--- ScriptWordPressAWS.sh
+++ ScriptWordPressAWS.sh
@@ -54,4 +54,4 @@ sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-
 sudo sed -i 's/database_name_here/wordpress/g' /var/www/html/wordpress/wp-config.php
 sudo sed -i 's/username_here/wp_admin/g' /var/www/html/wordpress/wp-config.php 
 sudo sed -i 's/password_here/root/g' /var/www/html/wordpress/wp-config.php
-sudo sed -i 's/localhost/$IPBD/g' /var/www/html/wordpress/wp-config.php
+sudo sed -i 's/localhost/${IPBD}/g' /var/www/html/wordpress/wp-config.php
